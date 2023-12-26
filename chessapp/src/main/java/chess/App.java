4bff0fd6939649661d1fbd9bf7d9bdf93597d1d1
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-12-26 17:30:24 +0100

    GUI View

--- chessapp/src/main/java/chess/App.java
+++ chessapp/src/main/java/chess/App.java
@@ -8,15 +8,18 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class App extends Application
+{
 
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage)
+    {
         ChessModel model = new ChessModel(); // Create your model
         ChessView view = new ChessView(); // Create your view
-        HumanVsComputerController controller = new HumanVsComputerController(view, model); // Create your controller
+        HumanVsComputerController controller = new HumanVsComputerController(view, model);
+        // Create your controller
 
         scene = new Scene(view, 800, 800); // Add the view to the scene. Adjust the size as needed
         stage.setScene(scene);
@@ -24,7 +27,8 @@ public class App extends Application {
         ChessModelTests tests = new ChessModelTests();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 
--- chessapp/src/main/java/chess/Views/GUIView.java
+++ chessapp/src/main/java/chess/Views/GUIView.java
@@ -1,5 +1,25 @@
 package chess.Views;
 
-public class GUIView {
-    
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+public class GUIView extends BorderPane {
+    private ChessView chessView;
+    private Button button1, button2; // Add more buttons as needed
+
+    public GUIView(ChessView chessView) {
+        this.chessView = chessView;
+
+        // Create buttons for different game modes
+        button1 = new Button("Game Mode 1");
+        button2 = new Button("Game Mode 2");
+        // Add buttons to a horizontal box
+        HBox hbox = new HBox(button1, button2);
+
+        // Add the chess view and buttons to this GUI view
+        this.setCenter(chessView);
+        this.setBottom(hbox);
+
+    }
 }
