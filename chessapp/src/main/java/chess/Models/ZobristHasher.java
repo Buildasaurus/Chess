a245b27b7e28bbe5c45725a5e0aaf9b2df59c71a
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2024-01-20 20:22:29 +0100

    Create ZobristHasher.java

---
+++ chessapp/src/main/java/chess/Models/ZobristHasher.java
@@ -0,0 +1,20 @@
+package chess.Models;
+
+// Static class
+// Assumes board is of size 64.
+public class ZobristHasher
+{
+
+
+    int[][][] randomZobrist;
+
+    static void initalizeValues()
+    {
+        int[][][] randomZobrist = new int[8][8][18];
+        for (int i = 0; i < randomZobrist.length; i++) {
+            for (int j = 0; j < randomZobrist.length; j++) {
+
+            }
+        }
+    }
+}
